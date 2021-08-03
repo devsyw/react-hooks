d07e50d1492de724565a64d59d097bf48e866720
Youngwoo Seo <devsyw@gmail.com>
2021-08-03 10:47:06 +0900

    useEffect 연습

--- react-hooks-2021/src/App.js
+++ react-hooks-2021/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 
 const contents = [
   {
@@ -31,47 +31,63 @@ const contents = [
 //   return { value, onChange };
 // }
 
-const useTabs = (initalTab, allTabs) => {
-  const [currentIndex, setCurrentIndex] = useState(initalTab);
-  if(!allTabs || !Array.isArray(allTabs)) return;
-  return {
-    currentItem : allTabs[currentIndex],
-    changeItem : setCurrentIndex
-  };
+// const useTabs = (initalTab, allTabs) => {
+//   const [currentIndex, setCurrentIndex] = useState(initalTab);
+//   if(!allTabs || !Array.isArray(allTabs)) return;
+//   return {
+//     currentItem : allTabs[currentIndex],
+//     changeItem : setCurrentIndex
+//   };
+// }
 
-}
+// const App = () => {
+//   //const maxLen2 = value => value.length <= 10; /* 입력범위 제한 */
+//   const maxLen = value => !value.includes("@"); /* 특수문제 제외 */
+
+//   const [count, setCount] = useState(0);
+//   const [email, setEmail] = useState("");
+
+//   const updateEmail = e => {
+//     const {target : {value}} = e;
+//     setEmail(value);
+//   }
+
+//   const { currentItem, changeItem } = useTabs(0, contents);
+
+//   return (
+//     <>
+//       {/* <input className="name-input" placeholder="name" {...name}/> <br/> */}
+//       {count} <br/>
+//       <button className="btn-blue" onaClick={ () => setCount(count + 1) }>inc</button>
+//       <button className="btn-blue" onClick={ () => setCount(count - 1) }>dec</button> <br/>
+//       <input className="mail-input" placeholder="Email" value={email} onChange={updateEmail}/>
+
+//       <div className="App">
+//         {contents.map((section, index) => (
+//           <button onClick={() => changeItem(index)}>{section.tab}</button>
+//         ))}
+//         <div>{currentItem.content}</div>
+//       </div>
+//     </>
+
+//   );
+// };
 
 const App = () => {
-  //const maxLen2 = value => value.length <= 10; /* 입력범위 제한 */
-  const maxLen = value => !value.includes("@"); /* 특수문제 제외 */
-
-  const [count, setCount] = useState(0);
-  const [email, setEmail] = useState("");
-
-  const updateEmail = e => {
-    const {target : {value}} = e;
-    setEmail(value);
-  }
-
-  const { currentItem, changeItem } = useTabs(0, contents);
+  const sayHello = () => console.log("hello");
+  
+  const [number, setNumber] = useState(0);
+  const [aNumber, setAnumber] = useState(0);
+  useEffect(sayHello, [number]);
+  useEffect(sayHello, [aNumber]);
 
   return (
-    <>
-      {/* <input className="name-input" placeholder="name" {...name}/> <br/> */}
-      {count} <br/>
-      <button className="btn-blue" onaClick={ () => setCount(count + 1) }>inc</button>
-      <button className="btn-blue" onClick={ () => setCount(count - 1) }>dec</button> <br/>
-      <input className="mail-input" placeholder="Email" value={email} onChange={updateEmail}/>
-
-      <div className="App">
-        {contents.map((section, index) => (
-          <button onClick={() => changeItem(index)}>{section.tab}</button>
-        ))}
-        <div>{currentItem.content}</div>
-      </div>
-    </>
-
+    <div className="App">
+      <div>Hi</div>
+      <button onClick={() => setNumber(number+1)}>{number}</button>
+      <button onClick={() => setAnumber(aNumber+1)}>{aNumber}</button>
+    </div>
   );
-};
+}
 
 export default App;
